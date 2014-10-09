d83f93d8eb7c38370788e35ac5095d7df0e9bbce
TerminalTesting <testing@terminal.ru>
2014-10-09 15:55:36 +0600

    remove unused import

--- wiki_extr.py
+++ wiki_extr.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
 #delete old screenshot artifacts.
 os.system('find -iname \*.png -delete')
